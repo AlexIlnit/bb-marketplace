2c533b0ec4318988aae6c851caac1cd943a44d40
AlexIlnit <148810219+AlexIlnit@users.noreply.github.com>
2026-06-21 21:47:22 +0300

    fix perfomans 100  21/06/26 21.47

--- client-next/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="text-center p-20">
-      <h1 className="text-4xl font-bold">
-        Объявление не найдено
-      </h1>
-    </div>
-  );
-}
